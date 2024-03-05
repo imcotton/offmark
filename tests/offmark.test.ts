3285775361802bb22f558ddf1455aebe47f5192e
Cotton Hou <himcotton@gmail.com>
2024-03-06 01:32:41 +0800

    refine main with pipe

--- tests/offmark.test.ts
+++ tests/offmark.test.ts
@@ -1,21 +1,22 @@
 import * as ast from 'jsr:@std/assert';
 import { toText } from 'jsr:@std/streams';
 
-import { main } from '../src/offmark.ts';
+import {
+
+    main,
+    pipe,
+
+} from '../src/offmark.ts';
 
 
 
 
 
-Deno.test('main on sample.ts.md', async function () {
+Deno.test('pipe on sample.ts.md', async function () {
 
     using file = await Deno.open('./tests/sample.ts.md', { read: true });
 
-    const { readable, writable } = new TransformStream<Uint8Array>();
-
-    main(file.readable, writable);
-
-    const txt = await toText(readable);
+    const txt = await toText(pipe(file.readable));
 
     ast.assertStringIncludes(txt, 'number');
     ast.assertStringIncludes(txt, 'as const');
@@ -23,6 +24,36 @@ Deno.test('main on sample.ts.md', async function () {
     ast.assertNotMatch(txt, /Literate/);
     ast.assertNotMatch(txt, /Programming/);
     ast.assertNotMatch(txt, /results:/);
+
+});
+
+
+
+
+
+Deno.test('main', async function () {
+
+    await Promise.all([
+
+        main(ReadableStream.from([]), new WritableStream()),
+
+        ast.assertRejects(async function () {
+
+            await main(
+
+                new ReadableStream({
+                    pull () {
+                        throw new Error('wrong');
+                    }
+                }),
+
+                new WritableStream(),
+
+            );
+
+        }),
+
+    ]);
 
 });
 
