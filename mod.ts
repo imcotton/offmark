0f930403f7af6d2c39c37427cef99e81a4b92bfa
Cotton Hou <himcotton@gmail.com>
2025-05-27 22:58:29 +0800

    support import.meta.main other than Deno runtime

--- mod.ts
+++ mod.ts
@@ -4,11 +4,25 @@ export { OffmarkStream, pipe } from './offmark.ts';
 
 
 
-if (import.meta.main) {
+bin: if (import.meta.main) {
 
     const { main } = await import('./offmark.ts');
 
-    await main(Deno.stdin.readable, Deno.stdout.writable);
+    if (globalThis.Deno?.version?.deno) {
+
+        main(Deno.stdin.readable, Deno.stdout.writable);
+        break bin;
+
+    }
+
+    const {    stdin,   stdout } = await import('node:process');
+    const { Readable, Writable } = await import('node:stream');
+
+    main(
+        // @ts-ignore YOLO
+        Readable.toWeb(stdin),
+        Writable.toWeb(stdout),
+    );
 
 }
 
