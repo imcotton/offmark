952b2d272585c48129ebb80799e49e75138d4fe6
Cotton Hou <himcotton@gmail.com>
2025-06-01 20:42:09 +0800

    task of build-npm

---
+++ tools/build-npm.ts
@@ -0,0 +1,88 @@
+#!/usr/bin/env -S deno run --allow-run -E -R -W=./
+
+import { build, stop } from 'npm:esbuild@0.25.5';
+
+import * as fs from 'jsr:@std/fs@1';
+
+import { denoPlugins } from 'jsr:@luca/esbuild-deno-loader@0.11.1';
+
+import pkg from '../deno.json' with { type: 'json' };
+
+
+
+
+
+async function main () {
+
+    await fs.ensureDir('./dist');
+
+    await build({
+
+        entryPoints: [
+            './src/index.ts',
+        ],
+        outdir: './dist',
+        bundle: true,
+        legalComments: 'none',
+        format: 'esm',
+        platform: 'node',
+        target: [ 'node20', 'es2022' ],
+        plugins: [ ...denoPlugins() ],
+
+    }).then(stop);
+
+    await Promise.all([
+
+        Deno.readTextFile('./dist/index.js')
+
+            // const is fine
+            .then(replace_all(/^var /gm, 'const '))
+
+            // class is fine, too
+            .then(replace_all(/const (.*) = class/gm, 'class $1'))
+
+            .then(write_text_file('./dist/index.js'))
+
+        ,
+
+        Deno.readTextFile('./src/bin.ts')
+
+            // budget --rewriteRelativeImportExtensions
+            .then(replace_all(`.ts';`, `.js';`))
+
+            .then(write_text_file('./dist/bin.js'))
+
+        ,
+
+        Deno.readTextFile('./tmp-package.json')
+
+            .then(str => str.replace('0.0.0-VERSION', pkg.version))
+
+            .then(write_text_file('./package.json'))
+
+        ,
+
+    ]);
+
+}
+
+
+
+
+
+const write_text_file: (path: string) => (source: string) => Promise<void>
+= path => source => Deno.writeTextFile(path, source);
+
+const replace_all: (old: string | RegExp, next: string) => (origin: string) => string
+= (old, next) => origin => origin.replaceAll(old, next);
+
+
+
+
+
+if (import.meta.main) {
+
+    main();
+
+}
+
