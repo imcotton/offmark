3163a3f19f4ecfa822a3190bc75ee17d54c64fff
Cotton Hou <himcotton@gmail.com>
2025-06-03 03:34:46 +0800

    import main via ./index in mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -6,7 +6,7 @@ export * from './index.ts';
 
 if (import.meta.main) {
 
-    const { main } = await import('./offmark.ts');
+    const { main } = await import('./index.ts');
 
     await main(Deno.stdin.readable, Deno.stdout.writable);
 
