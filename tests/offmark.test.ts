dda68de0169f2344434d32a06629f2fb02bc47bb
Cotton Hou <himcotton@gmail.com>
2025-06-10 11:39:24 +0800

    pass opts to mixing with DEFAULT

--- tests/offmark.test.ts
+++ tests/offmark.test.ts
@@ -1,10 +1,12 @@
 import * as ast from 'jsr:@std/assert@1';
+import { dedent } from 'jsr:@std/text@^1.0.14/unstable-dedent';
 import { toText } from "@std/streams";
 
 import {
 
     main,
     pipe,
+    OffmarkStream,
 
 } from '../src/offmark.ts';
 
@@ -25,6 +27,51 @@ Deno.test('pipe on sample.ts.md', async function () {
     ast.assertNotMatch(txt, /Literate/);
     ast.assertNotMatch(txt, /Programming/);
     ast.assertNotMatch(txt, /results:/);
+
+});
+
+
+
+
+
+Deno.test('custom options of OffmarkStream', async function () {
+
+    const stream = ReadableStream.from([
+
+        'hello',
+
+        '```ts',
+        'const foo: number = 1;',
+        '```',
+
+        'world',
+
+        '    const indent: 2 = 2;',
+
+        'wat',
+
+        '```',
+        'const bar = 3;',
+        '```',
+
+        'footer',
+
+    ]);
+
+    const readable = stream.pipeThrough(new OffmarkStream({
+
+        toggle: (line, on) => on ? line.startsWith('```')
+                                 : line.startsWith('```ts')
+        ,
+
+    }));
+
+    const res = await Array.fromAsync(readable).then(arr => arr.join('\n'));
+
+    ast.assertStrictEquals(res, dedent`
+        const foo: number = 1;
+        const indent: 2 = 2;
+    `);
 
 });
 
